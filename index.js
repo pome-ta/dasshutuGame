d8e8ae1223b92fa99f4ccf9ad48a3ed353012412
play.js <git@gituser.com>
2020-01-17 09:48:55 +0900

    logic

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 'use static';
 
-let num = 'num';
-let despri = 'despri';
-let hint = 'hint';
+// 回答数管理
+let flag = 0
 
-console.log('hoge');
+let qnum = ` ${flag + 1} `;
+let qlabel = `第${qnum}問`;
+//console.log(qlabel);
+
+// 書き換え要素の コンストラクタ
+
+// イニシャライズ _待機画面(一問目)_
+
+// イニシャライズから if で丸めて分岐を管理？
 
 
+/* dom操作用関数
 
+# id 取得
+let element = document.getElementById(id);
 
+# id テキスト
+## 文字列そのもの
+let text = element.textContent;
+element.textContent = "this is some sample text";
 
+## タグも含めて
+const content = element.innerHTML;
+element.innerHTML = htmlString;
 
-function Panel(_num, _despri, _hint) {
-  this.num = _num,
-  this.despri = _despri,
-  this.hint =_hint,
-}
+> 文書に HTML を挿入するという場合には、`insertAdjacentHTML()`メソッドを使用
 
-let panel(num,despri,hint) =new Panel();
-console.log(panel);
+*/
